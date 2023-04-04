89824285f45719ca3b862ac28f01157fd8b3306e
Mikalai Lappo <mikalailappo@yandex.by>
2023-04-04 22:02:40 +0300

    refactor: initial websocket rewrite

--- src/components/websocket.ts
+++ src/components/websocket.ts
@@ -1,11 +1,15 @@
 /** WS SERVER */
+interface DefilerWebsocketOptions {
+    serverPath: string;
+    shortTimeout: number;
+    shortTries: number;
+    longTimeout: number;
+    pingPeriod: number;
+    debug: boolean;
+} 
 
-export default function DefilerWebSocketServer(options) {
-    this.init(options)
-}
-
-DefilerWebSocketServer.defaultOptions = {
-    serverPath: false,
+const defaultOptions = {
+    serverPath: undefined,
     shortTimeout: 500,
     shortTries: 5,
     longTimeout: 60000,
@@ -13,15 +17,29 @@ DefilerWebSocketServer.defaultOptions = {
     debug: false,
 }
 
-DefilerWebSocketServer.prototype = {
-    init: function (options) {
-        this.options = Object.assign({}, DefilerWebSocketServer.defaultOptions, options);
+type SocketHandler = (...arg: any) => void
+type SocketEvent = 'Open' | 'Message' | 'Close' | 'Error' | 'Ping' | 'Pong'
 
-        this.stCounter = 0
-        this.connectTime = false
-        this.reconnectPointer = null
-        this.pingPongPointer = null
-        this.ping = '?'
+export default class DefilerWebSocketServer {
+    options: DefilerWebsocketOptions
+    handlers: {
+        onOpen: SocketHandler[]
+        onMessage: SocketHandler[]
+        onClose: SocketHandler[]
+        onError: SocketHandler[]
+        onPing: SocketHandler[]
+        onPong: SocketHandler[]
+    }
+    stCounter: number
+    ping: string
+    server?: WebSocket
+    connectTime?: number
+    pingStartTime?: number
+    reconnectPointer?: number
+    pingPongPointer?: number
+
+    constructor(options: DefilerWebsocketOptions) {
+        this.options = { ...defaultOptions, ...options }
         this.handlers = {
             onOpen: [],
             onMessage: [],
@@ -30,116 +48,115 @@ DefilerWebSocketServer.prototype = {
             onPing: [],
             onPong: [],
         }
+        this.stCounter = 0
+        this.ping = '?'
 
-        if (this.serverPath === false) {
+        this.init()
+    }
+
+    init() {
+        if (!this.options.serverPath) {
             console.error('Kirov reporting: server path is not defined')
             return false
         }
 
         this.connect()
-    },
-
+    }
     /** service */
 
-    connected: function () {
+    connected() {
         return this.server && this.server.readyState !== WebSocket.CLOSED
-    },
+    }
 
-    getPing: function () {
+    getPing() {
         if (!this.connected()) return 'dead'
         return this.ping
-    },
+    }
 
-    getLifeTime: function () {
-        return this.connectTime !== false ? Date.now() - this.connectTime : false
-    },
+    getLifeTime() {
+        return this.connectTime ? Date.now() - this.connectTime : undefined
+    }
 
-    addListener: function (eventType, handler) {
-        if (typeof handler === 'function') {
-            this.handlers[eventType].push(handler)
-            return true
-        }
-        return false
-    },
+    addListener(eventType: SocketEvent, handler: SocketHandler) {
+        if (!this.handlers[`on${eventType}`]) return false
+        this.handlers[`on${eventType}`].push(handler)
+        return true
+    }
 
     /** system */
 
-    send: function (message) {
-        if (this.server && this.server.readyState !== WebSocket.CLOSED) {
-            if (message === 'ping') {
-                this.pingStartTime = Date.now()
-                for (let i = 0; i < this.handlers.onPing.length; i++)
-                    this.handlers.onPing[i]()
-            }
-            this.server.send(typeof message === 'object' ? JSON.stringify(message) : message)
-            return true
+    send(message: string) {
+        if (!this.server || this.server.readyState === WebSocket.CLOSED) {
+            return false
         }
-        return false
-    },
+        if (message === 'ping') {
+            this.pingStartTime = Date.now()
+            this.handlers.onPing.forEach(pingCb => pingCb())
+        }
+        this.server.send(typeof message === 'object' ? JSON.stringify(message) : message)
+        return true
+    }
 
-    connect: function () {
+    connect() {
         this.server = new WebSocket(this.options.serverPath)
         this.server.onopen = this.onOpen.bind(this)
         this.server.onclose = this.onClose.bind(this)
         this.server.onmessage = this.onMessage.bind(this)
         this.server.onerror = this.onError.bind(this)
-    },
+    }
 
-    onMessage: function (event) {
+    onMessage(event) {
         if (this.options.debug) console.log(event.data)
         if (event.data === 'pong') {
-            if (this.pingStartTime !== false) {
-                this.ping = Date.now() - this.pingStartTime
+            if (this.pingStartTime !== undefined) {
+                this.ping = `${Date.now() - this.pingStartTime}`
                 for (let i = 0; i < this.handlers.onPong.length; i++)
                     this.handlers.onPong[i](this.ping, this.getLifeTime())
                 if (this.options.debug) console.log('ping: ' + this.ping + 'ms')
             }
             if (this.options.pingPeriod > 0) {
                 clearTimeout(this.pingPongPointer)
-                this.pingPongPointer = setTimeout(function () {
-                    this.send('ping')
-                }.bind(this), this.options.pingPeriod)
+                this.pingPongPointer = window.setTimeout(() => this.send('ping'), this.options.pingPeriod)
             }
         }
         for (let i = 0; i < this.handlers.onMessage.length; i++)
             this.handlers.onMessage[i](event.data)
-    },
+    }
 
-    onOpen: function (event) {
+    onOpen(event) {
         this.ping = '?'
         this.connectTime = Date.now()
         this.stCounter = 0
         clearTimeout(this.reconnectPointer)
         console.log(`Kirov reporting: [${this.options.serverPath}] connected.`)
 
-        for (let i = 0; i < this.handlers.onOpen.length; i++)
-            this.handlers.onOpen[i]()
+        this.handlers.onOpen.forEach(openCb => openCb())
 
         if (this.options.pingPeriod > 0) this.send('ping')
-    },
+    }
 
-    onClose: function (event) {
+    onClose(event) {
         clearTimeout(this.pingPongPointer)
         const
             timeout = this.stCounter < this.options.shortTries ? this.options.shortTimeout : this.options.longTimeout
-        this.reconnectPointer = setTimeout(this.check.bind(this), timeout)
+        this.reconnectPointer = window.setTimeout(this.check.bind(this), timeout)
         this.stCounter < this.options.shortTries && this.stCounter++
         //console.log(`Kirov reporting: ${this.options.serverPath} is closed. Try to reconnect in ${timeout} ms..`, event.reason)
         console.log(`Kirov reporting: ${this.options.serverPath} is closed. Try to reconnect in ${timeout} ms..`)
 
         for (let i = 0; i < this.handlers.onClose.length; i++)
             this.handlers.onClose[i]()
-    },
+    }
 
-    onError: function (error) {
-        this.server.close()
+    onError(error) {
+        this.server?.close()
         //console.error(`Kirov reporting: our base is under attack!`);
 
         for (let i = 0; i < this.handlers.onError.length; i++)
             this.handlers.onError[i]()
-    },
+    }
 
-    check: function () {
+    check() {
         if (!this.server || this.server.readyState === WebSocket.CLOSED) this.connect()
     }
 }
