e2d8920aecc5f963059b806c4afc3d76a40f43b5
Mikalai Lappo <mikalailappo@yandex.by>
2023-04-13 21:27:16 +0300

    fix: ambient react jsx, tsconfig dist overlap

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
   env: {
-    browser: true
-  }
+    browser: true,
+  },
 };
