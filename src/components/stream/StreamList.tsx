9fb6b589136b884d3e290245e94b2d4011a8b779
Mikalai Lappo <mikalailappo@yandex.by>
2023-08-12 16:33:33 +0300

    refactor: decouple useInterval, direct elems map

--- src/components/stream/StreamList.tsx
+++ src/components/stream/StreamList.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useRef } from 'react';
-
 import { Link } from 'react-router-dom';
 
+import { useInterval } from '../../hooks/useInterval';
 import { IStream } from '../../types/stream';
 
 type IStreamListProps = {
@@ -15,7 +14,7 @@ const StreamList = ({
   currentStream,
   refreshData,
 }: IStreamListProps) => {
-  const sortedList = streams.slice().sort((a, b) => {
+  const sortedList = [...streams].sort((a, b) => {
     const typeIndex = (type) => {
       return ['defiler', 'progamer', 'other', 'heresy'].findIndex(
         (x) => x === type,
@@ -29,49 +28,6 @@ const StreamList = ({
     return ta - tb;
   });
 
-  const streamList = sortedList.map((stream, index) => {
-    const styles = stream.pic
-      ? {
-          backgroundImage: `url('/images/pic/${stream.pic}.png')`,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        }
-      : {};
-    return (
-      <Link to={'/stream/' + stream.id} key={index}>
-        <div
-          className={`stream-block ${
-            currentStream && currentStream.id === stream.id ? 'chosen' : null
-          }`}
-        >
-          <div className={`stream-icon ${stream.pic}`} style={styles}>
-            &nbsp;
-          </div>
-          <div className={`stream-race ${stream.race}`}>&nbsp;</div>
-          {stream.name}
-        </div>
-      </Link>
-    );
-  });
-
-  function useInterval(callback, delay) {
-    const savedCallback = useRef();
-    // Remember the latest function.
-    useEffect(() => {
-      savedCallback.current = callback;
-    }, [callback]);
-    // Set up the interval.
-    useEffect(() => {
-      function tick() {
-        if (savedCallback?.current) (savedCallback.current as () => unknown)();
-      }
-      if (delay !== null) {
-        const id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
-    }, [delay]);
-  }
-
   useInterval(() => {
     refreshData('streams', 'online');
   }, 60000);
@@ -79,7 +35,32 @@ const StreamList = ({
   return (
     <>
       <div className="stream-list-header"></div>
-      {streamList}
+      {sortedList.map((stream, index) => {
+        const styles = stream.pic
+          ? {
+              backgroundImage: `url('/images/pic/${stream.pic}.png')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+            }
+          : {};
+        return (
+          <Link to={'/stream/' + stream.id} key={index}>
+            <div
+              className={`stream-block ${
+                currentStream && currentStream.id === stream.id
+                  ? 'chosen'
+                  : null
+              }`}
+            >
+              <div className={`stream-icon ${stream.pic}`} style={styles}>
+                &nbsp;
+              </div>
+              <div className={`stream-race ${stream.race}`}>&nbsp;</div>
+              {stream.name}
+            </div>
+          </Link>
+        );
+      })}
       <div className="stream-list-footer"></div>
     </>
   );
---
+++ src/hooks/useInterval.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+
+export const useInterval = (callback: () => void, delay: number) => {
+  const savedCallback = useRef<null | (() => void)>(null);
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      if (savedCallback?.current) savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
